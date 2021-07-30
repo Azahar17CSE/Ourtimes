36ea53ba360edd331d148a836cc5d9aa75498f23
unknown <azahar17cse@gmail.com>
2021-07-31 00:40:52 +0600

    Initialize The Project and folder create

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Ourtimes</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="title">
+		<center>
+			<h1>We are Underconstructions!</h1>
+			<p>Something is better!We are ourtimes</p>
+		</center>
+	</div>
+</body>
+</html>
